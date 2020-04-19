ef2eea90e0fc8b5ac9479bc339b6baebc2b67db2
Alexander Svistunov <Svistunov.A.Ev@sberbank.ru>
2020-04-19 18:39:58 +0300

    extend transform stream by adding limit parameter for data

--- 2-module/1-task/LimitSizeStream.js
+++ 2-module/1-task/LimitSizeStream.js
@@ -4,21 +4,17 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
-
-    this._limit = options.limit;
-    this._data = '';
+    this.limit = options.limit;
+    this.dataSize = null;
   }
 
   _transform(chunk, encoding, callback) {
-    this._data += chunk;
-
-    if (Buffer.byteLength(this._data) <= this._limit) {
-      this.push(chunk);
-    } else {
+    this.dataSize += chunk.length;
+    if (this.dataSize > this.limit) {
       callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
     }
-
-    callback(null);
   }
 }
 
