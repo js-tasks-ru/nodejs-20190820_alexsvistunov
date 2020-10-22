c0a5f629145141d31e7af8f36bf7c3a03691c427
Alexander Svistunov <Svistunov.A.Ev@sberbank.ru>
2020-10-22 15:46:57 +0300

    implement in file-server removing files

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -12,10 +13,38 @@ server.on('request', (req, res) => {
   switch (req.method) {
     case 'DELETE':
 
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('forbidden');
+        return;
+      }
+
+      fs.stat(filepath, (err, stat) => {
+        if (err) {
+          console.log(err.message);
+
+          res.statusCode = 404;
+          res.end('not founded');
+          return;
+        }
+
+        fs.unlink(filepath, (err) => {
+          if (err) {
+            res.statusCode = 501;
+            res.end('Unknown error');
+            console.log(err.message);
+            return;
+          }
+
+          res.statusCode = 200;
+          res.end('ok!');
+        });
+      });
+
       break;
 
     default:
-      res.statusCode = 501;
+      res.statusCode = 500;
       res.end('Not implemented');
   }
 });
