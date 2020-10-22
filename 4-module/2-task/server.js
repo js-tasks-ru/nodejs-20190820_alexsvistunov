c975d6f0d7f0adb9f4d0e68b80b7b2c2520404c9
Alexander Svistunov <Svistunov.A.Ev@sberbank.ru>
2020-10-22 15:26:19 +0300

    file-server for writing files

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,16 +1,82 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const {parse} = require('querystring');
+
+const FILE_LIMIT = 1e6;
 
 const server = new http.Server();
+
+const writeFile = (path, data, response) => {
+  fs.writeFile(path, data, {flag: 'wx'}, (err) => {
+    if (err) {
+      response.end(err.message);
+      return;
+    }
+
+    response.statusCode = 201;
+    response.end(`ok!`);
+  });
+};
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
+  const filesDirectoryPath = path.join(__dirname, 'files');
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('forbidden');
+        return;
+      }
+
+      if (+req.headers['content-length'] > FILE_LIMIT) {
+        res.statusCode = 413;
+        res.end('body is too big!');
+        return;
+      }
+
+      let body = '';
+      let size = 0;
+
+      req.on('data', (chunk) => {
+        if (size > FILE_LIMIT) {
+          res.statusCode = 413;
+          res.end('body is too big!');
+        } else {
+          body += chunk.toString();
+          size += chunk.length;
+        }
+      });
+
+      req.on('end', () => {
+        fs.stat(filesDirectoryPath, (err, stat) => {
+          if (err) {
+            fs.mkdir(filesDirectoryPath, (err) => {
+              if (err) throw err;
+
+              writeFile(filepath, JSON.stringify(parse(body)), res);
+            });
+          } else {
+            fs.stat(filepath, (err, stats) => {
+              if (err) {
+                writeFile(filepath, JSON.stringify(parse(body)), res);
+              } else {
+                res.statusCode = 409;
+                res.end('File already existed!');
+              }
+            });
+          }
+        });
+      });
+
+      req.on('error', (err) => {
+        console.log(err.message);
+      });
 
       break;
 
