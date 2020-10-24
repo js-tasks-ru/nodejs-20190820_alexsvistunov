25edc0d0ebbb531da2cd986e62e1478c2e15e23a
Alexander Svistunov <Svistunov.A.Ev@sberbank.ru>
2020-10-24 17:19:52 +0300

    made simple chat: added receiving and sending messages to subscribers

--- 5-module/1-task/app.js
+++ 5-module/1-task/app.js
@@ -7,10 +7,32 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const subscriptions = [];
+
 router.get('/subscribe', async (ctx, next) => {
+  const getNewMessage = () => new Promise((resolve, reject) => {
+    subscriptions.push(resolve);
+
+    ctx.res.on('close', () => {
+      const resolveIndex = subscriptions.indexOf(resolve);
+      subscriptions.splice(resolveIndex, 1);
+    });
+  });
+
+  ctx.response.body = await getNewMessage();
 });
 
 router.post('/publish', async (ctx, next) => {
+  const newMessage = ctx.request.body.message;
+
+  if (newMessage) {
+    subscriptions.forEach((resolve) => resolve(newMessage));
+    ctx.response.status = 200;
+    ctx.response.body = 'OK';
+  } else {
+    ctx.response.status = 204;
+    ctx.response.body = 'No Content';
+  }
 });
 
 app.use(router.routes());
